e815ea5be178fe858f642b514516faf8ed96e951
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-24 17:48:33 +0200

    conferma dei numeri

--- js/main.js
+++ js/main.js
@@ -5,63 +5,70 @@ const fourthNumber = document.getElementById("fourth-number");
 const fifthNumber = document.getElementById("fifth-number");
 const confirmButton = document.getElementById("confirm-button");
 
-const firstRandom = firstNumber.setAttribute(
-	"value",
-	parseInt(Math.round(Math.random() * 100))
-);
-const secondRandom = secondNumber.setAttribute(
-	"value",
-	parseInt(Math.round(Math.random() * 100))
-);
-const thirdRandom = thirdNumber.setAttribute(
-	"value",
-	parseInt(Math.round(Math.random() * 100))
-);
-const fourthRandom = fourthNumber.setAttribute(
-	"value",
-	parseInt(Math.round(Math.random() * 100))
-);
-const fifthRandom = fifthNumber.setAttribute(
-	"value",
-	parseInt(Math.round(Math.random() * 100))
-);
+//creo i numeri random
+const firstRandom = Math.floor(Math.random() * 100);
+const secondRandom = Math.floor(Math.random() * 100);
+const thirdRandom = Math.floor(Math.random() * 100);
+const fourthRandom = Math.floor(Math.random() * 100);
+const fifthRandom = Math.floor(Math.random() * 100);
+
+//imposto i valori negli input in html
+firstNumber.value = firstRandom;
+secondNumber.value = secondRandom;
+thirdNumber.value = thirdRandom;
+fourthNumber.value = fourthRandom;
+fifthNumber.value = fifthRandom;
 
 //if (firstNumber.value === secondNumber.value) {
 //	parseInt(Math.floor(Math.random() * 100));
 //}
 
-console.log(
-	firstNumber.value,
-	secondNumber.value,
-	thirdNumber.value,
-	fourthNumber.value,
-	fifthNumber.value
-);
-
+//funzione per cancellare gli input random
 function clearInput() {
-	firstNumber.setAttribute("value", "");
-	secondNumber.setAttribute("value", "");
-	thirdNumber.setAttribute("value", "");
-	fourthNumber.setAttribute("value", "");
-	fifthNumber.setAttribute("value", "");
+	firstNumber.value = "";
+	secondNumber.value = "";
+	thirdNumber.value = "";
+	fourthNumber.value = "";
+	fifthNumber.value = "";
 }
 
+//setto un timeout per far scomparire gli imput dopo tot tempo
 setTimeout(clearInput, 3000);
-
-console.log(
-	firstNumber.value,
-	secondNumber.value,
-	thirdNumber.value,
-	fourthNumber.value,
-	fifthNumber.value
-);
 
 //alert("Inserisci i numeri che hai visto sullo schermo");
 
 confirmButton.addEventListener("click", () => {
-	if (firstNumber.value === firstRandom) {
+	if (firstNumber.value === firstRandom.toString()) {
 		alert("il primo numero è corretto");
 	} else {
 		alert("il primo numero è sbagliato");
 	}
+	if (secondNumber.value === secondRandom.toString()) {
+		alert("il secondo numero è corretto");
+	} else {
+		alert("il secondo numero è sbagliato");
+	}
+	if (thirdNumber.value === thirdRandom.toString()) {
+		alert("il terzo numero è corretto");
+	} else {
+		alert("il terzo numero è sbagliato");
+	}
+	if (fourthNumber.value === fourthRandom.toString()) {
+		alert("il quarto numero è corretto");
+	} else {
+		alert("il quarto numero è sbagliato");
+	}
+	if (fifthNumber.value === fifthRandom.toString()) {
+		alert("il quinto numero è corretto");
+	} else {
+		alert("il quinto numero è sbagliato");
+	}
 });
+
+console.log(
+	firstNumber.value,
+	secondNumber.value,
+	thirdNumber.value,
+	fourthNumber.value,
+	fifthNumber.value
+);
