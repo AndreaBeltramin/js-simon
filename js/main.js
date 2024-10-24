76408bf24028a4e673cba8c8f75beb4a7e7a88e5
AndreaBeltramin <andreabeltra99@gmail.com>
2024-10-24 17:05:29 +0200

    valori utente

--- js/main.js
+++ js/main.js
@@ -1,20 +1,50 @@
-let firstNumber = document.getElementById("first-number");
-let secondNumber = document.getElementById("second-number");
-let thirdNumber = document.getElementById("third-number");
-let fourthNumber = document.getElementById("fourth-number");
-let fifthNumber = document.getElementById("fifth-number");
+const firstNumber = document.getElementById("first-number");
+const secondNumber = document.getElementById("second-number");
+const thirdNumber = document.getElementById("third-number");
+const fourthNumber = document.getElementById("fourth-number");
+const fifthNumber = document.getElementById("fifth-number");
+const confirmButton = document.getElementById("confirm-button");
 
-firstNumber.setAttribute("value", parseInt(Math.floor(Math.random() * 100)));
-secondNumber.setAttribute("value", parseInt(Math.floor(Math.random() * 100)));
-thirdNumber.setAttribute("value", parseInt(Math.floor(Math.random() * 100)));
-fourthNumber.setAttribute("value", parseInt(Math.floor(Math.random() * 100)));
-fifthNumber.setAttribute("value", parseInt(Math.floor(Math.random() * 100)));
+console.log(
+	firstNumber.value,
+	secondNumber.value,
+	thirdNumber.value,
+	fourthNumber.value,
+	fifthNumber.value
+);
 
-if (firstNumber.value === secondNumber.value) {
-	parseInt(Math.floor(Math.random() * 100));
-}
+const firstRandom = firstNumber.setAttribute(
+	"value",
+	parseInt(Math.floor(Math.random() * 100))
+);
+const secondRandom = secondNumber.setAttribute(
+	"value",
+	parseInt(Math.floor(Math.random() * 100))
+);
+const thirdRandom = thirdNumber.setAttribute(
+	"value",
+	parseInt(Math.floor(Math.random() * 100))
+);
+const fourthRandom = fourthNumber.setAttribute(
+	"value",
+	parseInt(Math.floor(Math.random() * 100))
+);
+const fifthRandom = fifthNumber.setAttribute(
+	"value",
+	parseInt(Math.floor(Math.random() * 100))
+);
 
-setTimeout(clearInput, 3000);
+//if (firstNumber.value === secondNumber.value) {
+//	parseInt(Math.floor(Math.random() * 100));
+//}
+
+console.log(
+	firstNumber.value,
+	secondNumber.value,
+	thirdNumber.value,
+	fourthNumber.value,
+	fifthNumber.value
+);
 
 function clearInput() {
 	firstNumber.setAttribute("value", "");
@@ -23,3 +53,23 @@ function clearInput() {
 	fourthNumber.setAttribute("value", "");
 	fifthNumber.setAttribute("value", "");
 }
+
+setTimeout(clearInput, 3000);
+
+console.log(
+	firstNumber.value,
+	secondNumber.value,
+	thirdNumber.value,
+	fourthNumber.value,
+	fifthNumber.value
+);
+
+//alert("Inserisci i numeri che hai visto sullo schermo");
+
+confirmButton.addEventListener("click", () => {
+	if (firstNumber.value === firstRandom) {
+		alert("il primo numero è corretto");
+	} else {
+		alert("il primo numero è sbagliato");
+	}
+});
